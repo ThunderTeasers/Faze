5f661f8895738a0754a0c2f0f2307bbedadd827b
ThunderTeasers <wowgrack@gmail.com>
2018-09-24 00:17:30 +0300

    Инициализация модуля тултипа и объявление глобальной переменной PlarsonJS чтобы сборщик не минифицировал это имя

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import PlarsonJS from './components/Core/PlarsonJS';
 import Tooltip from './components/Tooltip/Tooltip';
 
-new Tooltip(document.querySelector('#button'), {
-  side: 'top',
-  text: 'Проверочный текст',
+// @ts-ignore
+window.PlarsonJS = PlarsonJS;
+
+/**
+ * Инициализация заводских плагинов
+ */
+PlarsonJS.add({
+  pluginName: 'Tooltip',
+  callback: () => {
+    PlarsonJS.Tooltip = Tooltip;
+  },
 });

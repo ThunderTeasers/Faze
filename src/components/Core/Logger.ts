680d439ab1e12706650672a093114561cf586040
ThunderTeasers <wowgrack@gmail.com>
2019-03-08 02:35:51 +0300

    Модуль Logger: добавлен модуль логирования

---
+++ src/components/Core/Logger.ts
@@ -0,0 +1,29 @@
+/**
+ * Класс для упрощения логирования и вывода ошибок в консоль
+ *
+ * Автор: Ерохин Максим, plarson.ru
+ * Дата: 08.03.2019
+ */
+class Logger {
+  // То что будет приписываться до текста
+  prefix: string;
+
+  // То что будет приписываться после текста
+  postfix: string;
+
+  constructor(prefix: string = '', postfix: string = '') {
+    this.prefix = prefix;
+    this.postfix = postfix;
+  }
+
+  /**
+   * Вывод ошибки в консоль и выброс исключения
+   *
+   * @param message - сообщение которое необходимо отобразить
+   */
+  error(message: string) {
+    throw new Error(`${this.prefix} ${message} ${this.postfix}`);
+  }
+}
+
+export default Logger;

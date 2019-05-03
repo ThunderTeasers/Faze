6386227547d645cf5137aaf8a8b0642d59167ba1
ThunderTeasers <wowgrack@gmail.com>
2019-05-03 15:18:43 +0300

    Модуль Observer: добавлены комментарии, исправлен метод "check" теперь работает более быстро за счет оптимизации работы с "listener.alreadyExistedNodes"

--- src/components/Core/Observer.ts
+++ src/components/Core/Observer.ts
@@ -1,18 +1,42 @@
+/**
+ * Модуль ядра - Observer
+ *
+ * Предоставляет возможность автоматической инициализации плагинов по data атрибутам при динамическом создании DOM элементов на странице
+ *
+ * Автор: Ерохин Максим, plarson.ru
+ * Дата: 03.05.2019
+ */
+
+/**
+ * Структура слушателя плагина
+ *
+ * Содержит:
+ *   selector             - CSS селектор DOM элемента который надо отслеживать
+ *   callback             - пользовательский метод, исполняющийся при добавлении нового элемента с указанным селектором,
+ *                          передает его DOM элемент
+ *   alreadyExistedNodes  - список уже существующих DOM элементов с указанным селектором
+ */
 interface Listener {
   selector: string;
   callback: (addedNode: HTMLElement) => void;
-  alreadyExistedNodes: NodeListOf<Element>;
+  alreadyExistedNodes: HTMLElement[];
 }
 
+/**
+ * Класс Observer
+ */
 class Observer {
+  // Список слушателей
   readonly listeners: Listener[];
 
+  // Основной объект observer'а
   mutationObserver: MutationObserver;
 
   constructor() {
     // Инициализация переменных
     this.listeners = [];
 
+    // Инициализируем сам observer
     this.mutationObserver = new MutationObserver(this.check.bind(this));
     this.mutationObserver.observe(document.documentElement, {
       childList: true,
@@ -20,15 +44,26 @@ class Observer {
     });
   }
 
+  /**
+   * Добавление слушателя к общему списку
+   *
+   * @param selector - CSS селектор DOM элемента для отслеживания
+   * @param callback - пользовательский метод, исполняющийся после добавления нужного DOM элемента
+   */
   ready(selector: string, callback: (addedNode: HTMLElement) => void) {
     this.listeners.push({
       selector,
       callback,
-      alreadyExistedNodes: document.querySelectorAll(selector),
+      alreadyExistedNodes: Array.from(document.querySelectorAll(selector)),
     });
   }
 
-  check(mutationRecords: MutationRecord[]) {
+  /**
+   * Отслеживание всех изменений DOM элементов на сайте
+   *
+   * @param mutationRecords - список изменения
+   */
+  private check(mutationRecords: MutationRecord[]) {
     // Проходимся по всем слушателям
     this.listeners.forEach((listener) => {
       // Проходимся по всем изменениям
@@ -52,11 +87,22 @@ class Observer {
 
                   // Обновляем уже существующие элементы у слушателя, чтобы при следующем добавлении элемента, пользовательская
                   // функция срабатывала только на последний, а не на все которые были добавлены после инициализации
-                  listener.alreadyExistedNodes = document.querySelectorAll(listener.selector);
+                  // listener.alreadyExistedNodes = document.querySelectorAll(listener.selector);
+                  listener.alreadyExistedNodes.push(<HTMLElement>insertedElement);
                 }
               }
             });
           }
+        });
+
+        // Проходимся по всем удаленным DOM элементам и убираем их из массива существующих элементов слушателя, чтобы не засорять его и
+        // очистить память
+        mutationRecord.removedNodes.forEach((removedNode: Node) => {
+          listener.alreadyExistedNodes.forEach((existedNode: HTMLElement) => {
+            if (existedNode === removedNode) {
+              listener.alreadyExistedNodes = listener.alreadyExistedNodes.filter(node => node !== existedNode);
+            }
+          });
         });
       });
     });

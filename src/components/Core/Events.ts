ac4ea3d12787dfd421ddc9220979b0e60764fcaf
ThunderTeasers <erokhinmaksim.personal@gmail.com>
2023-07-21 16:15:31 +0300

    Модуль Events: Добавлен модуль "forEach" для удобного навешивания событий на массив DOM элементов

--- src/components/Core/Events.ts
+++ src/components/Core/Events.ts
@@ -49,6 +49,35 @@ class Events {
       }
     });
   }
+
+  /**
+   * Навешивание событие на массив DOM элементов со всеми проверками
+   *
+   * @param type Тип события
+   * @param nodesOrSelector DOM элементы на которые навешиваем событие или его CSS селектор
+   * @param callback Пользовательская функция исполняющаяся после события
+   * @param isPreventDefault Нужно ли делать "preventDefault()" у события
+   */
+  static forEach(type: string, nodesOrSelector: HTMLElement[] | string, callback: (event: Event, node: HTMLElement | null) => void, isPreventDefault: boolean = true): void {
+    // Проверяем, является ли переданный параметр строкой, если да,
+    // то ищём соответствующий DOM элемент по селектору, если нет, используем напрямую
+    let nodes: HTMLElement[];
+    if (typeof nodesOrSelector === 'string' || nodesOrSelector instanceof String) {
+      nodes = [...document.querySelectorAll<HTMLElement>(nodesOrSelector as string)];
+    } else {
+      nodes = nodesOrSelector;
+    }
+
+    // Проверка на существование DOM элементов
+    if (nodes.length === 0) {
+      return;
+    }
+
+    // Навешиваем события на все элементы в массиве
+    nodes.forEach((node) => {
+      this.listener(type, node, callback, isPreventDefault);
+    });
+  }
 }
 
 export default Events;

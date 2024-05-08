9a3b2749daef85190c198df1c6e99c6527b4c5c6
ThunderTeasers <thunderteasers@yandex.ru>
2024-05-08 13:16:45 +0300

    Модуль Tour: Инициализация и базовая настройка шагов

--- src/components/Modules/Tour/Tour.ts
+++ src/components/Modules/Tour/Tour.ts
@@ -4,9 +4,8 @@
  * Модуль табов представляет собой набор "заголовков" связанных с набором "тел" через data атрибут. Одновременно может отображаться
  * только одно "тело", какое именно отображается, зависит от того, на какой "заголовок" нажали. По умолчанию отображается первый.
  *
- * Автор: Ерохин Максим, plarson.ru
- * Дата: 26.09.2018
- * Документация: https://github.com/ThunderTeasers/Faze/wiki/Модуль-Tab
+ * Автор: Ерохин Максим
+ * Дата: 08.05.2024
  */
 
 import './Tour.scss';
@@ -16,24 +15,28 @@ import Module from '../../Core/Module';
  * Структура возвращаемого объекта в пользовательском методе
  *
  * Содержит:
- *   currentStep - Текущий шаг
+ *   group - Группа
  */
 interface CallbackData {
-  currentStep: number;
+  group: string;
+}
+
+interface StepData {
+  index: number;
+  text: string;
+  side: 'left' | 'right' | 'bottom' | 'top';
 }
 
 /**
  * Структура конфига табов
  *
  * Содержит:
- *   headerActiveClass - CSS класс активного таба
- *   useHash - использовать ли window.location.hash при переключении и инициализации
- *   removeEmpty - удалять ли пустые табы с их шапками
- *   maxBodies - максимальное количество тел которые нужно показывать
+ *   group - Группа
  *   callbacks
  *     changed - пользовательская функция, исполняющаяся после изменения таба
  */
 interface Config {
+  group: string;
   callbacks: {
     changed?: (activeTabData: CallbackData) => void;
   };
@@ -43,6 +46,9 @@ interface Config {
  * Класс табов
  */
 class Tour extends Module {
+  // Данные шагов
+  private _stepsData: StepData[];
+
   /**
    * Стандартный конструктор
    *
@@ -52,6 +58,7 @@ class Tour extends Module {
   constructor(node?: HTMLElement, config?: Partial<Config>) {
     // Конфиг по умолчанию
     const defaultConfig: Config = {
+      group: 'default',
       callbacks: {
         changed: undefined,
       },
@@ -70,12 +77,45 @@ class Tour extends Module {
    */
   protected initialize(): void {
     super.initialize();
+
+    this.initializeSteps();
+
+    console.log(this._stepsData);
+  }
+
+  /**
+   * Инициализация шагов тура для дальнейшей работы
+   *
+   * @private
+   */
+  private initializeSteps(): void {
+    this._stepsData = Array.from(this.node.querySelectorAll<HTMLElement>(`[data-faze-tour-step="${this.config.group}"]`))
+      .map(
+        (stepNode, index) =>
+          ({
+            index: parseInt(stepNode.dataset.fazeTourStepIndex || index.toString(), 10),
+            text: stepNode.dataset.fazeTourStepText || '',
+            side: stepNode.dataset.fazeTourStepSide || 'right',
+          } as StepData)
+      )
+      .sort((stepA, stepB) => stepA.index - stepB.index);
   }
 
   /**
    * Навешивание событий
    */
   protected bind(): void {}
+
+  /**
+   * Инициализация модуля по data атрибутам
+   *
+   * @param node - DOM элемент на который нужно инициализировать плагин
+   */
+  static initializeByDataAttributes(node: HTMLElement): void {
+    new Tour(node, {
+      group: node.dataset.fazeTourGroup,
+    });
+  }
 }
 
 export default Tour;

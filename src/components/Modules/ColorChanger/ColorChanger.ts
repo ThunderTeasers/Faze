90d64b952ad922f34e53038ad4e0b71fddeb7e9e
ThunderTeasers <thunderteasers@yandex.ru>
2024-10-08 00:55:57 +0300

    Модуль ColorChanger: Добавлена базовая инициализация

--- src/components/Modules/ColorChanger/ColorChanger.ts
+++ src/components/Modules/ColorChanger/ColorChanger.ts
@@ -4,3 +4,70 @@
  * Автор: Ерохин Максим
  * Дата: 08.10.2024
  */
+
+import './ColorChanger.scss';
+import Module from '../../Core/Module';
+
+/**
+ * Структура возвращаемого объекта в пользовательском методе
+ *
+ * Содержит:
+ *   itemNode - DOM элемент где происходит работа модуля
+ *   colorsNode - DOM элемент холдера цветов
+ *   colorsNodes - DOM элементы цветов
+ *   selectedColorNode - DOM элемент выбранного цвета
+ */
+interface CallbackData {
+  itemNode: HTMLElement;
+  colorsNode: HTMLElement;
+  colorsNodes: HTMLElement[];
+  selectedColorNode: HTMLElement;
+}
+
+/**
+ * Структура конфига табов
+ *
+ * Содержит:
+ *   changeOnHover - изменять ли цвет при наведении(по умолчанию смена идёт при клике)
+ *   shortColors - количество цветов в свёрнутом варианте
+ *   callbacks
+ *     changed - пользовательская функция, исполняющаяся после изменения таба
+ */
+interface Config {
+  changeOnHover: boolean;
+  quantity: number;
+  callbacks: {
+    changed?: (data: CallbackData) => void;
+  };
+}
+
+/**
+ * Класс переключателя цветов
+ */
+class ColorChanger extends Module {
+  /**
+   * Стандартный конструктор
+   *
+   * @param node DOM элемент на который навешивается модуль
+   * @param config Конфиг модуля
+   */
+  constructor(node?: HTMLElement, config?: Partial<Config>) {
+    // Конфиг по умолчанию
+    const defaultConfig: Config = {
+      changeOnHover: false,
+      quantity: 4,
+      callbacks: {
+        changed: undefined,
+      },
+    };
+
+    // Инициализируем базовый класс
+    super({
+      node,
+      config: Object.assign(defaultConfig, config),
+      name: 'ColorChanger',
+    });
+  }
+}
+
+export default ColorChanger;

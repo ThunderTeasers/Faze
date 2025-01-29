96430c28969043e468e682712090487a5181f0eb
ThunderTeasers <thunderteasers@yandex.ru>
2025-01-29 13:35:45 +0300

    Модуль DOM: Добавлен метод "insertBefore"

--- src/components/Helpers/DOM.ts
+++ src/components/Helpers/DOM.ts
@@ -11,6 +11,16 @@ class DOM {
   static insertAfter(node: HTMLElement, referenceNode: HTMLElement): void {
     referenceNode.parentNode?.insertBefore(node, referenceNode.nextSibling);
   }
+
+  /**
+   * Вставка DOM элемента до указанного
+   *
+   * @param node{HTMLElement} DOM элемент который вставляем
+   * @param referenceNode{HTMLElement} DOM элемент до которого вставляем
+   */
+  static insertBefore(node: HTMLElement, referenceNode: HTMLElement): void {
+    node.parentNode?.insertBefore(referenceNode, node);
+  }
 }
 
 export default DOM;
